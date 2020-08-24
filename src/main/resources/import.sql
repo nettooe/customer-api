4be23fc69a50e25b0940101e5b7ae6a97e4b6698
Oliver Netto <netto.oe@gmail.com>
2020-08-23 23:01:14 -0300

    Changing from H2 to MySql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO user(id, username, password) VALUES (1, 'admin', 'OyR+CCJbfhuC0soKon/P3a7J4YTpL3o8LG8FgG3YeLk=');
-INSERT INTO user(id, username, password) VALUES (2, 'user', 'u3FOP4rW7dZRx8I8FqwHfgRDu1/9yIzZO+etM0IliNc=');
+INSERT INTO user(username, password) VALUES ('admin', 'OyR+CCJbfhuC0soKon/P3a7J4YTpL3o8LG8FgG3YeLk=');
+INSERT INTO user(username, password) VALUES ('user', 'u3FOP4rW7dZRx8I8FqwHfgRDu1/9yIzZO+etM0IliNc=');
 
-insert INTO user_role(user_id, role_name) VALUES (1, 'ADMIN');
-insert INTO user_role(user_id, role_name) VALUES (2, 'USER');
+insert INTO user_role(user_id, role_name) VALUES ( (SELECT u.id FROM user u WHERE u.username = 'admin') , 'ADMIN');
+insert INTO user_role(user_id, role_name) VALUES ( (SELECT u.id FROM user u WHERE u.username = 'user')  , 'USER');
